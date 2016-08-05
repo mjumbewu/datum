9da32f1a5dc66f50b7383bdae348289103f1931f
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-08-05 09:55:34 -0400

    Add installation requirements to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,14 @@ setup(name='datum',
       author_email='maps@phila.gov',
       license='MIT',
       packages=['datum'],
+      install_requires=[
+        'pyproj==1.9.5.1',
+        'Shapely==1.5.15',
+        'six==1.10.0',
+      ],
+      extras_require={
+        'oracle': ['cx_Oracle==5.2.1'],
+        'postgres': ['psycopg2==2.6.1'],
+      },
       # entry_points={'console_scripts': ['ais=ais:manager.run']},
       zip_safe=False)
